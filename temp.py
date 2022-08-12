d5ff83a8a803665fdcdc48fd7f581c6bdfc15a38
Phung Xuan Anh <phungxuananh1991@gmail.com>
2022-08-12 16:54:51 +0700

    update python version

--- temp.py
+++ temp.py
@@ -1,3 +1,7 @@
 aws ecr create-repository --repository-name flask-app-fargate-fargate --region ap-southeast-1
 
 aws ecr get-login --region ap-southeast-1 --no-include-email
+
+https://aws.amazon.com/blogs/architecture/field-notes-integrating-http-apis-with-aws-cloud-map-and-amazon-ecs-services/
+
+
